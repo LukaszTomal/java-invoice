1c2d9cd35caf2b8ba50d7fe0c0fb7902670b6e57
tomal <tomal_l@poczta.onet.pl>
2022-02-05 16:05:20 +0100

    Part of tests for Invoice were done

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,43 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+//    private Collection<Product> products = new ArrayList<>();
 
-    public void addProduct(Product product) {
-        // TODO: implement
+    private Map<Product, Integer> products = new HashMap<>();
+
+    public void addProduct(Product product){
+        this.addProduct(product, 1);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        this.products.put(product, quantity);
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+        BigDecimal sum = BigDecimal.ZERO;
+        for(Product product : this.products.keySet()){
+            Integer quantity = this.products.get(product);
+            BigDecimal quantityAsBigDecimal = BigDecimal.valueOf(quantity);
+
+            sum = sum.add(product.getPrice().multiply(quantityAsBigDecimal);
+        }
+        return sum;
+
     }
 
     public BigDecimal getTax() {
-        return null;
+        return BigDecimal.ZERO;
     }
 
     public BigDecimal getTotal() {
-        return null;
+        return BigDecimal.ZERO;
     }
 }
